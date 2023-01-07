7d6df2cc8bcd364a2d9460c4e572adf7e96743fa
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-01-07 13:00:08 -0300

    comment: more capybara selectors like comment

--- features/step_definitions/buscando_elementos.rb
+++ features/step_definitions/buscando_elementos.rb
@@ -4,10 +4,22 @@ Quando('acesso a url da página inicial') do
 end
   
 Então('verifico se encontrei os botões "Como se proteger"') do
-    #all busca todos os elementos que contenham o all
+    #all busca todos os elementos que contenham o all. Por padrao já opta pelo css
     page.all(:css, '.bt')
-    #find busca um elemento mapeado
-    # find_link(href: "/faq") 
+    # first, nesse caso, primeiro elemento que possua a classe informada.
     first('.bt')
+    #busca um elemento pelo link, no exemplo, pelo href
+    find_link(href: "/faq#contratar")
 
 end
+
+# OUTROS SELETORES CSS:
+
+#find busca um elemento mapeado, no exemplo, por um id
+  # find('#nome_id')
+#busca um elemento por um id
+  # find_by_id('nome_id')
+#busca um elemento por botton mas um paremetro, no exemplo, uma classe
+  # find_button(class: 'nome_classe')
+#busca um elemento pelo link, no exemplo, pelo href
+  # find_link(href: "/faq") 
